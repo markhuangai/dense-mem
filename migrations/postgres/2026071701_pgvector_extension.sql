e82fb771553b0e8c9fccab06cb2296d754d40722
Z-M-Huang <25124355+Z-M-Huang@users.noreply.github.com>
2026-07-17 23:03:09 +0000

    Address upstream bootstrap review feedback

--- migrations/postgres/2026071701_pgvector_extension.sql
+++ migrations/postgres/2026071701_pgvector_extension.sql
@@ -1,7 +1,19 @@
 -- +goose Up
 -- +goose StatementBegin
 
-CREATE EXTENSION IF NOT EXISTS vector;
+DO $$
+BEGIN
+	IF EXISTS (
+		SELECT 1
+		FROM pg_available_extensions
+		WHERE name = 'vector'
+	) THEN
+		CREATE EXTENSION IF NOT EXISTS vector;
+	END IF;
+EXCEPTION
+	WHEN insufficient_privilege THEN
+		RAISE NOTICE 'pgvector extension is available but current role cannot install it; V2 readiness will report missing pgvector when required';
+END $$;
 
 -- +goose StatementEnd
 
